5682db78bec8f453a0f3266f2c68331017ed7fd6
Christian Maniewski <code@chmanie.com>
2021-05-05 00:03:13 +0200

    Add uart output via NUCLEO usb

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,9 @@ use cortex_m;
 use cortex_m_rt::entry;
 use stm32f4xx_hal as hal;
 
-use crate::hal::{prelude::*, stm32};
+use crate::hal::{prelude::*, serial::config::Config, serial::Serial, stm32};
+
+use core::fmt::Write;
 
 #[entry]
 fn main() -> ! {
@@ -21,19 +23,37 @@ fn main() -> ! {
         let gpioa = dp.GPIOA.split();
         let mut led = gpioa.pa5.into_push_pull_output();
 
+        let tx_pin = gpioa.pa2.into_alternate_af7();
+        let rx_pin = gpioa.pa3.into_alternate_af7();
+
         // Set up the system clock. We want to run at 84MHz for this one.
         let rcc = dp.RCC.constrain();
         let clocks = rcc.cfgr.sysclk(84.mhz()).freeze();
+
+        let serial = Serial::usart2(
+            dp.USART2,
+            (tx_pin, rx_pin),
+            Config::default().baudrate(115_200.bps()),
+            clocks,
+        )
+        .unwrap();
+
+        let (mut tx, mut _rx) = serial.split();
 
         // Create a delay abstraction based on SysTick
         let mut delay = hal::delay::Delay::new(cp.SYST, clocks);
 
         loop {
             // On for 1s, off for 1s.
-            led.set_high().unwrap();
-            delay.delay_ms(200_u32);
-            led.set_low().unwrap();
-            delay.delay_ms(200_u32);
+            if led.set_high().is_err() {
+                // Handle error?
+            }
+            delay.delay_ms(1000_u32);
+            if led.set_low().is_err() {
+                // Handle error?
+            }
+            delay.delay_ms(1000_u32);
+            writeln!(tx, "Hello, world!\r",).unwrap();
         }
     }
 
